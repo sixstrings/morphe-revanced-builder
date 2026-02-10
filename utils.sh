340097bf86bdde23a51b767c813f30d2066c7532
Wolfcity <132693162+sixstrings@users.noreply.github.com>
2026-02-10 22:12:32 +0100

    Update utils.sh
    author 
    
    Signed-off-by: Wolfcity <132693162+sixstrings@users.noreply.github.com>

--- utils.sh
+++ utils.sh
@@ -755,7 +755,7 @@ module_prop() {
 name=${2}
 version=${3}
 versionCode=${NEXT_VER_CODE}
-author=peternmuller
+author=sixstrings 
 description=${4}" >"${6}/module.prop"
 
 	if [ "$ENABLE_MAGISK_UPDATE" = true ]; then echo "updateJson=${5}" >>"${6}/module.prop"; fi
